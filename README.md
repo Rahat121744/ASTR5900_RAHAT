a0f62af500dae1c66f898b13d0972ad09b4aa3e9
Rahat <md.rhat.ali-1@gmail.com>
2026-01-31 13:00:40 -0800

    line created

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 Computational Physics &amp; Astrophysics Spring 2026
 <br>
 Name-(Md Rahat Ali)
+<br>
 Norman,Oklahoma
+<br>
 Print"hello everyone"
+<br>
 a=4
 b=6
+<br>
 print(a-b)
 
--- my new file.py
+++ my new file.py
@@ -1,6 +1,6 @@
-print"assalamualaikum"
-print" this is for commit"
+print("assalamualaikum")
+print("this is for commit")
 a=5
 b=6
 print(a+b)
-print"finish HW00"
+print("finish HW00")
